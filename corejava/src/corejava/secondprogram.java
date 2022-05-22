8e752cf161c4ed3b3a17d346bd24018bdb6035db
Arun <102899417+CodeArtist12@users.noreply.github.com>
2022-05-22 21:29:48 +0530

    Add files via upload

---
+++ corejava/src/corejava/secondprogram.java
@@ -0,0 +1,21 @@
+package threadpg;
+
+class withdraw
+{
+	int amt_withdraw=2000;
+}
+
+public class  Day1_P1 extends withdraw{
+	
+	int amt_left=5000;
+	
+   	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		Day1_P1 ob= new  Day1_P1();
+System.out.println("amount withdraw"+ob1.amt_withdraw);
+System.out.println("amount_left"+ob1.amt_left);
+
+	}
+
+}
